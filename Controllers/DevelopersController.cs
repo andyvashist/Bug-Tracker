927c65dca7d216f1295bffba8448e97b4e9855fb
Andy <anandit46@gmail.com>
2020-04-05 17:18:00 -0700

    Minor UI Changes

--- Controllers/DevelopersController.cs
+++ Controllers/DevelopersController.cs
@@ -6,100 +6,107 @@ using System.Web.Mvc;
 
 namespace BugTracker.Controllers
 {
-    public class ProjectController : Controller
+    public class DevelopersController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-        public ProjectController()
-        {
-            db = new ApplicationDbContext();
-        }
 
+        // GET: Developer
         public ActionResult Index()
         {
-            return View(db.Projects.ToList());
+            return View(db.Developers.Include(d => d.Projects).ToList());
         }
 
+        // GET: Developer/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            var project = db.Projects.Find(id);
-            if (project == null)
+            Developer developer = db.Developers.Find(id);
+            if (developer == null)
             {
                 return HttpNotFound();
             }
-            return View(project);
+            return View(developer);
         }
 
+        // GET: Developer/Create
         public ActionResult Create()
         {
             return View();
         }
 
-
+        // POST: Developer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Project project)
+        public ActionResult Create(Developer developer)
         {
             if (ModelState.IsValid)
             {
-                db.Projects.Add(project);
+                db.Developers.Add(developer);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(project);
+            return View(developer);
         }
 
+        // GET: Developer/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Project project = db.Projects.Find(id);
-            if (project == null)
+            Developer developer = db.Developers.Find(id);
+            if (developer == null)
             {
                 return HttpNotFound();
             }
-            return View(project);
+            return View(developer);
         }
 
+        // POST: Developer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(Project project)
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName")] Developer developer)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(project).State = EntityState.Modified;
+                db.Entry(developer).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(project);
+            return View(developer);
         }
 
+        // GET: Developer/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Project project = db.Projects.Find(id);
-            if (project == null)
+            Developer developer = db.Developers.Find(id);
+            if (developer == null)
             {
                 return HttpNotFound();
             }
-            return View(project);
+            return View(developer);
         }
 
+        // POST: Developer/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Project project = db.Projects.Find(id);
-            db.Projects.Remove(project);
+            Developer developer = db.Developers.Find(id);
+            db.Developers.Remove(developer);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -6,107 +6,101 @@ using System.Web.Mvc;
 
 namespace BugTracker.Controllers
 {
-    public class DeveloperController : Controller
+    public class ProjectsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-
-        // GET: Developer
-        public ActionResult Index()
+        public ProjectsController()
         {
-            return View(db.Developers.Include(d => d.Projects).ToList());
+            db = new ApplicationDbContext();
         }
 
-        // GET: Developer/Details/5
+        public ActionResult Index()
+        {
+            return View(db.Projects.ToList());
+        }
+
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Developer developer = db.Developers.Find(id);
-            if (developer == null)
+            var project = db.Projects.Find(id);
+            project.Tickets = db.Tickets.Where(t => t.ProjectId == id).ToList();
+            if (project == null)
             {
                 return HttpNotFound();
             }
-            return View(developer);
+            return View(project);
         }
 
-        // GET: Developer/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Developer/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Developer developer)
+        public ActionResult Create(Project project)
         {
             if (ModelState.IsValid)
             {
-                db.Developers.Add(developer);
+                db.Projects.Add(project);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(developer);
+            return View(project);
         }
 
-        // GET: Developer/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Developer developer = db.Developers.Find(id);
-            if (developer == null)
+            Project project = db.Projects.Find(id);
+            if (project == null)
             {
                 return HttpNotFound();
             }
-            return View(developer);
+            return View(project);
         }
 
-        // POST: Developer/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName")] Developer developer)
+        public ActionResult Edit(Project project)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(developer).State = EntityState.Modified;
+                db.Entry(project).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(developer);
+            return View(project);
         }
 
-        // GET: Developer/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Developer developer = db.Developers.Find(id);
-            if (developer == null)
+            Project project = db.Projects.Find(id);
+            if (project == null)
             {
                 return HttpNotFound();
             }
-            return View(developer);
+            return View(project);
         }
 
-        // POST: Developer/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Developer developer = db.Developers.Find(id);
-            db.Developers.Remove(developer);
+            Project project = db.Projects.Find(id);
+            db.Projects.Remove(project);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
